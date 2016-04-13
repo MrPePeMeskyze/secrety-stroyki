e07e7a020bd0a1d713fd9b0c289bc55521a6cf76
Sergey <predatorqwerty2@gmail.com>
2016-04-13 11:57:49 +0300

    [-] Разбивка на паршелы

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+	def full_title(page_title)
+		standart_title = '"Секреты Стройки" - cтроительные услуги.'
+	if page_title.empty?
+		standart_title
+	else
+		"#{page_title}"
+	end
 end
