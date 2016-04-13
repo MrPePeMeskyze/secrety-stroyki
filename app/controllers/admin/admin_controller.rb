8c09ac70faaf8f0b573cb96e9063a66e067f6b29
Sergey <predatorqwerty2@gmail.com>
2016-04-13 11:09:07 +0300

    [-] Настройка имен контролеров и вьюх

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -1,4 +1,4 @@
-class Admin::ApplicationController < ActionController::Base
+class Admin::AdminController < ActionController::Base
   http_basic_authenticate_with name: "admin", password: "secret"
 
   layout "admin/layouts/application"
