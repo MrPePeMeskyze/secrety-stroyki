18241e1c76e91fdde84cea2e8b05756db45ff422
Sergey <predatorqwerty2@gmail.com>
2016-04-19 23:29:30 +0300

    [+] Роутинг по статическим страницам

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,7 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
+ENV['STATIC_PAGES_ID'] = "3"
+
 # Initialize the Rails application.
 Rails.application.initialize!
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,25 @@
 Rails.application.routes.draw do
-	root :to => "pages#index"
 	# admin area
 	get "/admin" => "admin/objects#index"
 	namespace :admin do
-	  resources :objects
+		resources :objects
 	end
 	namespace :admin do
-	  resources :objects_types
+		resources :objects_types
 	end
 	namespace :admin do
-	  resources :navigations
+		resources :navigations
 	end
+
+	## Выборка пермалинков для статических страниц
+	@__static = Objects.where("objects_type_id = ?", ENV['STATIC_PAGES_ID'])
+    @__static = (@__static.map do |_static| "#{_static.permalink}" end).join('|')
+
+	root :to => "pages#index"
+	get "/catalog/" => "catalog#index"
+
+	## Роутинг по статическим страницам
+	match ':id', via: [:get, :post], :controller => 'pages', :action => 'pages',  :constraints => {:id => /.*(#{@__static}).*/}
+
 	get ':controller(/:action(/:id))'
 end
