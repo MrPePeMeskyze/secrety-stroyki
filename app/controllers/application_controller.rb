0f65b589e6485556227a2fd4d99f3a5250ae3640
Sergey <predatorqwerty2@gmail.com>
2016-04-19 23:36:07 +0300

    [+] Доработка контролера приложения для обработки объектов

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_filter :init
+
+  private
+  	def init
+  	## TODO: Перехват на 404, вызов exception
+  	end
 end
