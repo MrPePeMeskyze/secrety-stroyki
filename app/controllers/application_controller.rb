8996b616cdde01d6c1c55b1c2ddb91797a5029dd
Sergey <predatorqwerty2@gmail.com>
2016-04-20 14:24:32 +0300

    [+] Перезапуск роутинга, при каждом обращении

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :rebuild_routes
   def not_found
     render :template => '404', :status => 404
   end
+
+  private
+    def rebuild_routes
+      Rails.application.routes_reloader.reload!
+    end
 end
