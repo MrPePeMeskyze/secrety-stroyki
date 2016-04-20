cc3450dae3e0c25546704bfb762f3dbde358788a
Sergey <predatorqwerty2@gmail.com>
2016-04-20 11:53:05 +0300

    [+] Ловец 404 ошибки для объектов и всех неизвестных страниц вне роутинга

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_filter :init
-
-  private
-  	def init
-  	## TODO: Перехват на 404, вызов exception
-  	end
+  def not_found
+    render :template => '404', :status => 404
+  end
 end
