8a5d853fa3f3c80c13e9784519422ec2e2101bf9
Sergey <predatorqwerty2@gmail.com>
2016-04-21 11:12:36 +0300

    [+] Криптирование пароля в md5, валидаторы, ассоциации

--- app/models/users.rb
+++ app/models/users.rb
@@ -1,3 +1,21 @@
 class Users < ActiveRecord::Base
 
+	validates :login, presence: true, length: { maximum: 50 }, uniqueness: true
+	validates :password, length: { minimum: 6 }
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+
+	has_many :sessions
+
+	before_save :before_save
+
+	private
+		def before_save
+			self.email = self.email.downcase
+			self.login = self.login.downcase
+
+			if(self.password? && self.password != self.password_was)
+				self.password = Digest::MD5.hexdigest(self.password)
+			end
+		end
 end
--- db/migrate/20160420125155_create_users.rb
+++ db/migrate/20160420125155_create_users.rb
@@ -3,7 +3,7 @@ class CreateUsers < ActiveRecord::Migration
     create_table :users do |t|
       t.string :login, index: true
       t.string :email, index: true
-      t.string :password_digest
+      t.string :password
       t.boolean :is_admin 
       t.boolean :is_published
       t.string :name
